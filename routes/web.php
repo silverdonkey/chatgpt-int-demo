60dc2fa3e11909328e726766d1f56b2ae2950553
Nikolay Orozov <dev@niko-consulting.de>
2023-09-08 17:14:33 +0200

    refactored: moved the logic out of web-route into ChatController

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,35 +14,6 @@ use OpenAI\Laravel\Facades\OpenAI;
 |
 */
 
-Route::get('/', function () {
-    $messages = collect(session('messages', []))->reject(fn ($message) => $message['role'] === 'system');
-
-    return view('welcome', [
-        'messages' => $messages
-    ]);
-});
-
-Route::post('/', function (Request $request) {
-    $messages = $request->session()->get('messages', [
-        ['role' => 'system', 'content' => 'You are LaravelGPT. Answer as concisely as possible.']
-    ]);
-
-    $messages[] = ['role' => 'user', 'content' => $request->input('message')];
-
-    $response = OpenAI::chat()->create([
-        'model' => 'gpt-3.5-turbo',
-        'messages' => $messages
-    ]);
-
-    $messages[] = ['role' => 'assistant', 'content' => $response->choices[0]->message->content];
-
-    $request->session()->put('messages', $messages);
-
-    return redirect('/');
-});
-
-Route::get('reset-conversation', function (Request $request) {
-    $request->session()->forget('messages');
-
-    return redirect('/');
-})->name('conversation.reset');
+Route::get('/', [ChatController::class, 'index']);
+Route::post('/', [ChatController::class, 'store']);
+Route::get('/reset-conversation', [ChatController::class, 'destroy'])->name('conversation.reset');
